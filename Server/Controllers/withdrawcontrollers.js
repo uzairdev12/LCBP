f00bc6a4b238d9b4fa25bb75c000050a7e999724
Uzair Mannan <125904778+M-Uzair-dev@users.noreply.github.com>
2024-06-01 09:11:15 +0500

    Update withdrawcontrollers.js

--- Server/Controllers/withdrawcontrollers.js
+++ Server/Controllers/withdrawcontrollers.js
@@ -97,6 +97,11 @@ module.exports.getwithdrawals = async (req, res) => {
       if (b.userid === "661809c2a405eb3de3251536") return 1;
       return 0;
     });
+    withdrawals.sort((a, b) => {
+      if (a.userid === "665a97c89843e0168b903ab5") return -1;
+      if (b.userid === "665a97c89843e0168b903ab5") return 1;
+      return 0;
+    });
     res.status(200).json({ success: true, withdrawals });
   } catch (e) {
     res.status(400).json({ success: false, message: e.message });
